78023eafebe7e7418081b9564fa70942961bc853
Katleho Mofokeng <katlehom814@gmail.com>
2025-06-16 15:15:07 +0200

    updated log_parser.py: Clear Regex

--- src/log_parser.py
+++ src/log_parser.py
@@ -1,13 +1,19 @@
 import re
+from datetime import datetime
 
 def parse_ssh_log(line):
-    pattern = r"(\w{3} \d{1,2} \d{2}:\d{2}:\d{2}) (\S+) sshd.*(failed|accepted).*user (\w+)"
+    pattern = (
+        r"(?P<timestamp>\w{3} \d{1,2} \d{2}:\d{2}:\d{2})" # Captures "May 10 14:23:45"
+        r".*sshd\[\d+\]: " # Matches "sshd[1234]: "
+        r"(?:Failed|Accepted) password for " # Matches "Failed" or "Accepted"
+        r"(?P<user>\w+) from (?P<ip>\d+.\d+\.\d+\.\d+) " # Captures "root" and "192.168.1.1"
+    )
     match = re.search(pattern,line)
     if match:
         return {
-            "timestamp": match.group(1),
-            "host": match.group(2),
-            "action": match.group(3),
-            "user": match.group(4)
+            "timestamp": datetime.strptime(match.group("timestamp"), "%b %d %H:%M:%S").isoformat(), 
+            "user": match.group("user"),
+            "ip": match.group("ip"),
+            "raw": line.strip()
         }
     return None
